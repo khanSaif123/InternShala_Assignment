38b33d04f8af8df6f5c8fe6d15d3c9d05b072a85
khanSaif123 <khansaiflko0@gmail.com>
2024-09-04 20:29:26 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
     <div className='w-[100%] h-[100vh] bg-[#303439] overflow-hidden relative flex flex-col'>
   
       <Card1/>
-      <div className='w-[630px] h-[5px] inline-block bg-[#303439] absolute top-[362px] mx-auto
-      right-[125px] rounded-3xl'
+      <div className='w-[630px]  h-[5px] inline-block bg-[#303439] absolute top-[362px] mx-auto
+      lg:right-[125px] md:right-[222px] rounded-3xl'
       style={{ boxShadow: '0px 3px 5px' }}
       ></div>
       <Card2/>
-      <div className='w-[630px] h-[5px] inline-block bg-[#303439] absolute bottom-[10px] mx-auto
-      right-[125px] rounded-3xl '
+      <div className='w-[630px] h-[5px] lg:inline-block md:hidden bg-[#303439] absolute bottom-[10px] mx-auto
+      right-[125px]  rounded-3xl '
       style={{ boxShadow: '0px 3px 5px' }}
       ></div>
     </div>
